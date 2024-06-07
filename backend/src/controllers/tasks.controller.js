6fc2f921ed6f40064f45a9473e0ff722023c4470
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-07 07:45:06 +0530

    added the get task by id route

--- backend/src/controllers/tasks.controller.js
+++ backend/src/controllers/tasks.controller.js
@@ -24,46 +24,4 @@ const getTaskById= asyncHandler(async (req, res, next) => {
     res.status(200).json(new ApiResponse(200, 'Task fetched successfully', task));
 });
 
-const createTask = asyncHandler(async (req, res, next) => { 
-    let {title,description,dueDate,status} = req.body;
-
-    const validate= createTaskSchema({title,description,dueDate,status});
-    if(!validate.success){
-        return res.status(400).json(new ApiError(400, validate.error.message, []))
-    }
-    const createdTask = await Todo.create({title,description,dueDate,status,owner:req.user});
-    res.status(201).json(new ApiResponse(201, 'Task created successfully', createdTask));
-})
-const updateTask = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    const {title,description,dueDate,status} = req.body;
-    
-    const validate= updateTaskSchema({title,description,dueDate,status});
-    if(!validate.success){
-        return res.status(400).json(new ApiError(400, validate.error.message, []))
-    }
-    
-    const task = await Todo.findById(id);   
-    if(!task){
-        return res.status(404).json(new ApiResponse(404, 'Task not found', []))
-    }
-    task.title = title===undefined?task.title:title;
-    task.description = description===undefined?task.description:description;
-    task.dueDate = dueDate===undefined?task.dueDate:dueDate;
-    task.status = status===undefined?task.status:status;
-    await task.save();
-
-    res.status(200).json(new ApiResponse(200, 'Task updated successfully', task));
-})
-
-const deleteTask = asyncHandler(async (req, res, next) => {
-    const {id} = req.params;
-    const task = await Todo.findById(id);
-    if(!task){
-        return res.status(404).json(new ApiError(404, 'Task not found', []))
-    }
-    await task.deleteOne();
-
-    res.status(200).json(new ApiResponse(200, 'Task deleted successfully', []));
-})
-export { getTasks,getTaskById,createTask,updateTask ,deleteTask}
+export { getTasks,getTaskById}
